813e14e17388fc27911ba9b821f8559f5dbf372f
unknown <asandilelangeni@gmail.com>
2023-11-02 15:50:17 +0200

    Fifth

--- mascripts/Main.js
+++ mascripts/Main.js
@@ -1,61 +1,68 @@
 
-window.onload = function() {
-const sneakersEl = document.querySelector('#products');
+const productsEl = document.querySelector(".products");
+console.log("After selecting productsEl:", productsEl);
 
-    function displayProduct() {
-        products.forEach((sneakers) => {
-           sneakersEl.innerHTML +=`
-    <div class="card m-auto" style="width: 18rem;">
-    <img src="${sneakers.image}" class="card-img-top" alt="sneaker">
-    <div class="card-body">
-        <h5 class="card-title">Name: ${sneakers.productName}</h5>
-        <p class="card-text">Instock: ${sneakers.instock}</p>
-        <p class="card-text"><small class="text-muted">Price: R${sneakers.price}</small></p>
-        <a href="#" class="btn btn-primary" id="check" onclick="addToCart(${sneakers.id})">Add</a>
-    </div>
-    </div>`;
-        })
+const cartItemsEl = document.querySelector(".cart-items");
+
+function displayProducts(){
+    products.forEach ((product) => {
+        productsEl.innerHTML += `
+        <div class="card m-auto" style="width: 18rem;">
+                <img src="${product.image}" class="card-img-top" alt="sneaker">
+                <div class="card-body">
+                    <h5 class="card-title">Name: ${product.productName}</h5>
+                    <p class="card-text">Instock: ${product.instock}</p>
+                    <p class="card-text"><small class="text-muted">Price: R${product.price}</small></p>
+                    <a href="#" class="btn btn-primary" id="check" onclick="addToCart(${product.id})">Add</a>
+                </div>
+            </div>`
         
-    };
+});
+}
 
-
-    
-    displayProduct();
+displayProducts();
 
 let cart = [];
-const cartItemsEl = document.getElementById('tafula');
 
-function addToCart(id) {
-    if (cart.some((item) => item.id === id)) {
-        alert("Product already added");
-    } else {
-        const item = products.find((product) => product.id === id);
+function addToCart(id){
+    console.log("Adding to cart:", id);
+    if(cart.some((item) => item.id === id)){
+        alert("product already added")
+    } else{
+        const item =  products.find(product => product.id === id);
+
         cart.push({
             ...item,
             quantity: 1,
         });
     }
-    updateCart();
+
+ updateCart();
 }
 
-function updateCart() {
+function cartItems(){
+    console.log("cartItemsEl:", cartItemsEl);
+    cartItemsEl.innerHTML = "Cart is empty";
+    cart.forEach((item) =>{
+        cartItemsEl.innerHTML += `
+        <div class="cart-item">
+        <div class="item-info">
+            <img src="${item.image}" alt="${item.productName}">
+            <h4>${item.productName}</h4>
+        </div>
+        <div class="unit-price">
+            <small>R</small>${item.price}
+        </div>
+        <div class="units">
+            <div class="btn-minus">-</div>
+            <div class="number">${item.quantity}</div>
+            <div class="btn-plus">+</div>
+        </div>
+    </div>`
+        
+    });
+}
+
+function updateCart(){
     cartItems();
 }
-
-function cartItems() {
-    cartItemsEl.innerHTML = "";
-    cart.forEach((item) => {
-        cartItemsEl.innerHTML += `
-        <tr>
-        <th scope="row">${item.id}</th>
-        <td>${item.productName}</td>
-        <td>${item.quantity}</td>
-        <td>${item.price}</td>
-      </tr>`;
-    });
-
-}
-
-}
-
-
